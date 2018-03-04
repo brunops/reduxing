6747d7826e661b3957459c7af3335c87033de42e
bsanches <bsanches@paypal.com>
2018-03-04 00:01:56 -0800

    Create rudimentary Redux Counter app
    - Concerns still need to be better separated inside folders...

--- src/client/index.js
+++ src/client/index.js
@@ -1,13 +1,68 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
+const rootReducer = require('./redux/reducers')
+// const App = require('./app')
+const { createStore, applyMiddleware } = require('redux')
+const logger = require('redux-logger').default
+const { Provider, connect } = require('react-redux')
 
-const App = () => {
+const store = createStore(
+  rootReducer,
+  applyMiddleware(logger)
+)
+
+
+const Counter = ({
+  counter,
+  increment,
+  decrement
+}) => {
+
+  console.log(increment)
+
   return (
-    <h1>Hello from React!</h1>
+    <div>
+      <h1>Counter: {counter} </h1>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+    </div>
   )
 }
 
+const increment = () => ({
+  type: 'INCREMENT'
+})
+
+const decrement = () => ({
+  type: 'DECREMENT'
+})
+
+const mapStateToProps = state => ({
+  counter: state.counter
+})
+
+const mapDispatchToProps = dispatch => ({
+  increment: () => dispatch(increment()),
+  decrement: () => dispatch(decrement())
+})
+
+const CounterContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Counter)
+
+const App = () => {
+  return (
+    <div id="app">
+      <CounterContainer />
+    </div>
+  )
+}
+
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('app')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('container')
 )
